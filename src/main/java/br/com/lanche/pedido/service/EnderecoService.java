ec96c5fb4a9b995b8b018ee6f3c652167d3febf8
Layanne <layrodrgs@gmail.com>
2022-10-08 16:26:18 -0300

    adding services

---
+++ src/main/java/br/com/lanche/pedido/service/EnderecoService.java
@@ -0,0 +1,4 @@
+package br.com.lanche.pedido.service;
+
+public interface EnderecoService {
+}
---
+++ src/main/java/br/com/lanche/pedido/service/PedidoService.java
@@ -0,0 +1,9 @@
+package br.com.lanche.pedido.service;
+
+import br.com.lanche.pedido.entity.Request.PedidoRequest;
+import br.com.lanche.pedido.entity.Response.PedidoResponse;
+
+public interface PedidoService {
+
+    PedidoResponse realizarPedido(final PedidoRequest pedido);
+}
